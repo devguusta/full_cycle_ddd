659081206cba85e2a3df2f0ac3be8ab22edf5bec
Gustavo Silva <gustavocsil60@gmail.com>
2024-04-28 13:35:15 -0300

    implement product repository

--- src/infrastructure/repository/product.repository.spec.ts
+++ src/infrastructure/repository/product.repository.spec.ts
@@ -1,5 +1,7 @@
 import { Sequelize } from "sequelize-typescript";
 import ProductModel from "../db/sequelize/model/product.model";
+import Product from "../../domain/entity/product";
+import ProductRepository from "./product.repository";
 
 describe("Product repository test", () => {
 
@@ -24,5 +26,97 @@ describe("Product repository test", () => {
         await sequelize.close();
     }
     );
+
+    it("should create a product", async () => {
+
+        const productRepository = new ProductRepository();
+
+        const product = new Product("p1", "Produto 1", 100);
+
+        await productRepository.create(product);
+       
+
+        const productModel = await ProductModel.findOne({ where: { id: product.id } });
+
+        expect(productModel.toJSON()).toStrictEqual(
+            {
+                id: "p1",
+                name: "Produto 1",
+                price: 100
+            }
+
+        )
+    });
+
+    it("should update a product", async () => {
+            
+            const productRepository = new ProductRepository();
+    
+            const product = new Product("p1", "Produto 1", 100);
+    
+            await productRepository.create(product);
+    
+            product.changeName("Produto 2");
+            product.changePrice(200);
+    
+            await productRepository.update(product);
+    
+            const productModel = await ProductModel.findOne({ where: { id: product.id } });
+    
+            expect(productModel.toJSON()).toStrictEqual(
+                {
+                    id: "p1",
+                    name: "Produto 2",
+                    price: 200
+                }
+    
+            )
+        });
+
+        it("should find a product", async () => {
+
+            const productRepository = new ProductRepository();
+    
+            const product = new Product("p1", "Produto 1", 100);
+    
+            await productRepository.create(product);
+    
+            const productFind = await productRepository.find("p1");
+    
+            expect(productFind).toStrictEqual(product);
+            
+        });
+
+        it("should delete a product", async () => {
+
+            const productRepository = new ProductRepository();
+    
+            const product = new Product("p1", "Produto 1", 100);
+    
+            await productRepository.create(product);
+    
+            await productRepository.delete("p1");
+    
+            const productModel = await ProductModel.findOne({ where: { id: product.id } });
+    
+            expect(productModel).toBeNull();
+            
+        });
+
+        it("should find all products", async () => {
+
+            const productRepository = new ProductRepository();
+    
+            const product1 = new Product("p1", "Produto 1", 100);
+            const product2 = new Product("p2", "Produto 2", 200);
+    
+            await productRepository.create(product1);
+            await productRepository.create(product2);
+    
+            const products = await productRepository.findAll();
+    
+            expect(products).toStrictEqual([product1, product2]);
+            
+        });
     
 });
---
+++ src/infrastructure/repository/product.repository.ts
@@ -0,0 +1,44 @@
+import { where } from "sequelize";
+import Product from "../../domain/entity/product";
+import ProductRepositoryInterface from "../../domain/repository/product-repository.interface";
+import ProductModel from "../db/sequelize/model/product.model";
+
+export default class ProductRepository implements ProductRepositoryInterface {
+  async  findAll(): Promise<Product[]> {
+        const productsModel = await ProductModel.findAll();
+        return productsModel.map(productModel =>
+             new Product(productModel.id, productModel.name, productModel.price));
+    }
+   async  find(id: string): Promise<Product> {
+        const productModel = await ProductModel.findOne({ where: { id: id } });
+        return new Product(productModel.id, productModel.name, productModel.price);
+    }
+   async create(data: Product): Promise<void> {
+    await ProductModel.create({
+ 
+        id: data.id,
+        name: data.name,
+        price: data.price
+    });
+    }
+   async  update(data: Product): Promise<void> {
+        await ProductModel.update({
+            name: data.name,
+            price: data.price
+        },
+            {
+                where: {
+                    id: data.id
+                }
+            });
+      
+    }
+    async delete(id: string): Promise<boolean> {
+        const productModel = await ProductModel.findOne({ where: { id: id } });
+        if (productModel) {
+            await productModel.destroy();
+            return true;
+        }
+        return false;
+    }
+}
